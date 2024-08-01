9e657cad807657b00182113c2a6d9e5962ca1a06
Adınız <you@example.com>
2024-08-01 14:11:00 +0300

    Açıklayıcı bir commit mesajı

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Post
 #super user modeli oluşturmamız lazım superuserin herşeye erişimi vardır
 #Admin sayfasında modelimizi görünür kılabilmek için, modeli admin.site.register(Post) ile kaydetmemiz gerekiyor.
-#venv etkinleştirmek için yürütme politikasını değiştir ardından myvenv\Scripts\activate bun kodu cmd ye yaz 
+#venv etkinleştirmek için yürütme politikasını değiştir ardından myvenv\Scripts\activate bun kodu cmd ye yaz dogru  dizine dikkat et
+#  
 
 admin.site.register(Post)
 
